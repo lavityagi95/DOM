611e5b1eb659b3be822d2a451b17fa64f467b110
lavityagi95 <lavityagiyt@gmail.com>
2026-07-28 00:12:39 +0530

    more working with dom

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const redBtn = document.getElementById("redBtn");
 const greenBtn = document.getElementById("greenBtn");
 const blueBtn = document.getElementById("blueBtn");
 const randomBtn = document.getElementById("randomBtn");
+const heading2 = document.getElementById("heading2")
+const btn = document.getElementById("btn")
+const nameInput = document.getElementById("nameInput")
+const message  = document.getElementById("message");
+const count  = document.getElementById("count")
+const words  = document.getElementById("words")
 
 
 function changeColor(color) {
@@ -22,7 +28,7 @@ blueBtn.addEventListener("click", () => {
     changeColor("blue");
 });
 
-const color = ["yellow","orange"];
+const color = ["yellow","orange","pink","purple"];
 
 const messages = [
     "Hello JavaScript",
@@ -44,3 +50,31 @@ function randomcolour(){
     heading.textContent = messages[index]
     
 }
+
+
+
+btn.addEventListener("click",()=>{
+
+    const name = nameInput.value.trim();
+    if(name == ""){
+        heading2.textContent = `Please enter your name`
+    }else{
+        heading2.textContent =  `Welcome ${name}`;
+
+    }
+    nameInput.value = ""
+})
+
+message.addEventListener("input",()=>{
+
+    count.textContent = message.value.length;
+
+    const words = message.value.trim() === ""
+        ? 0
+        : message.value.trim().split(/\s+/).length;
+
+    wordCount.textContent = words;
+
+})
+
+
